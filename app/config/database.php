fccac18cf88151263fc79494a3d205cb849baad5
haomaro <haomaro1706@gmail.com>
2014-08-29 01:43:06 -0500

    segunda
    subida

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'agenda',
+			'username'  => 'root',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
---
+++ app/controllers/users.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* 
+*/
+class Users_Controller extends Base_Controller
+{
+	
+	public function action_index()
+	{
+		return "Este es el controlador de usuarios";
+	}
+}
